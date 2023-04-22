7a1b1c87aa17bba1d56bb94eb6a64790072231bd
Alex Shab <alexandrshab@gmail.com>
2023-04-22 23:25:31 +0300

    base

--- classes/Connect.php
+++ classes/Connect.php
@@ -6,7 +6,7 @@ class Connect extends PDO
     {
         parent::__construct(
             "mysql:host=localhost; dbname=shinnymi_base",
-            'shinnymi_bot',
+            'shinnymi_site',
             'q6664091q===',
             array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4")
         );
